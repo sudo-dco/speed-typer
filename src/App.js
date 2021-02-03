3a5a198298f89babff3cb7ea2cd20fd15801dd4f
Daniel Co <dcohomemail@gmail.com>
2021-02-02 17:54:54 -0800

    moving state back into redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Sentence } from "./features/sentence/Sentence";
-import { TextInput } from "./features/textInput/TextInput";
+import { useKeyPress } from "./hooks/useKeyPress";
+import axios from "axios";
 
 function App() {
+    const [sentence, setSentence] = useState("");
+    const [preChars, setPreChars] = useState("");
+    const [postChars, setPostChars] = useState("");
+    const [currentChar, setCurrentChar] = useState();
+    const [textInput, setTextInput] = useState("");
+
+    // wpm
+    const [startTime, setStartTime] = useState(null);
+    const [wpm, setWpm] = useState(0);
+    const currentTime = () => new Date().getTime();
+
+    // incorrect text
+    const [isRed, setIsRed] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get("/sentence")
+            .then((response) => {
+                const sentence = response.data;
+
+                setSentence(sentence);
+                setCurrentChar(sentence[0]);
+                setPostChars(sentence.substring(1));
+            })
+            .catch((err) => {
+                console.log("error fetching sentence: ", err);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (startTime > 0 && textInput.length === sentence.length) {
+            const stopTime = currentTime();
+            const mins = (stopTime - startTime) / 60000;
+            const numOfChars = sentence.length;
+
+            const grossWpm = (numOfChars / 5 / mins).toFixed(2);
+            setWpm(grossWpm);
+        }
+    }, [textInput, sentence.length, startTime]);
+
+    const toggleColor = (status) => {
+        if (startTime !== null) {
+            if (status === true && isRed === false) {
+                setIsRed(true);
+            }
+
+            if (status === false && isRed === true) {
+                setIsRed(false);
+            }
+        }
+    };
+
+    let key = useKeyPress();
+    console.log(key);
+
+    // if key matches current char
+    if (key === currentChar) {
+        // on first correct key press, start timer
+        if (startTime === null) {
+            setStartTime(currentTime());
+        }
+
+        // remove incorrect class
+        toggleColor(false);
+
+        // append current char to prechars
+        setPreChars(preChars + currentChar);
+
+        // append to textinput
+        setTextInput(textInput + currentChar);
+
+        // get next current char from post char
+        setCurrentChar(postChars[0]);
+        setPostChars(postChars.substring(1));
+    } else if (key !== currentChar) {
+        toggleColor(true);
+    }
+
     return (
         <div className="App">
-            <Sentence />
-            <TextInput />
+            <div>{sentence}</div>
+            <div>Pre: {preChars}</div>
+            <input
+                type="text"
+                name="textInput"
+                value={textInput}
+                size="100"
+                className={isRed ? "textbox animate-red" : "textbox"}
+            />
+            <div>WPM: {wpm}</div>
         </div>
     );
 }
