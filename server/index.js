1fb0e84e5baabc14d0a6f8dd2f43c9e711a56e60
Daniel Co <dcohomemail@gmail.com>
2021-02-09 20:32:15 -0800

    add socket.io listeners

--- server/index.js
+++ server/index.js
@@ -1,15 +1,62 @@
 const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 const faker = require("faker");
 
-const app = express();
 const port = 5000;
+const rooms = [];
+
+const generateId = () => {
+    return Math.floor(Math.random() * 10000 + 1).toString();
+};
 
 app.use(express.static("build"));
 
+// routes
 app.get("/sentence", (req, res) => {
     res.send(faker.lorem.sentence());
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.get("/paragraph", (req, res) => {
+    res.send(faker.lorem.paragraph());
+});
+
+app.get("/create", (req, res) => {
+    let id = generateId();
+
+    if (rooms.includes(id)) {
+        id = generateId();
+    }
+
+    rooms.push(id);
+    res.send(id);
+});
+
+server.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
+
+// socket.io
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("set username", (name) => {
+        socket.username = name;
+    });
+
+    socket.on("create room", (name, roomId) => {
+        socket.join(roomId);
+        io.to(roomId).emit("update playerlist", name);
+    });
+
+    socket.on("join room", (name, roomId) => {
+        socket.join(roomId);
+        io.to(roomId).emit("update playerlist", name);
+    });
 });
